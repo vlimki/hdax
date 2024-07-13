6c5938e862d81f927082e1c86c066a31b11526e9
vlimki <vlimki@proton.me>
2024-07-13 18:47:06 +0300

    statistical functions on Series

--- src/Series.hs
+++ src/Series.hs
@@ -1,5 +1,17 @@
-module Series (Series) where
+module Series (Series, mean, median, length) where
 
-import Data.Vector as V
+import qualified Data.Vector as V
+import Prelude hiding (length)
 
 type Series a = V.Vector a
+
+length :: Series a -> Int
+length = V.length
+
+mean :: (Fractional a) => Series a -> a
+mean s = (1 / fromIntegral n) * V.sum s
+  where n = length s
+
+median :: Series a -> a
+median s = s V.! (n `div` 2)
+  where n = length s
